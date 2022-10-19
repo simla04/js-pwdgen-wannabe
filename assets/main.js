e52e99c4f6536a8f66c9f5bf461f87923a615616
simla04 <jessicasowi@gmail.com>
2022-10-19 17:14:40 +0200

    esercizio terminato

---
+++ assets/main.js
@@ -0,0 +1,22 @@
+let name = prompt(`Write your name:  `);
+//console.log('name')
+let surname = prompt ('Write your surname:  ');
+//console.log('surname')
+let color = prompt('Write your favorite color:  ');
+//console.log('color')
+let number = 30;
+
+console.log(name, surname, color)
+
+document.getElementById('mioDiv').innerHTML = `la tua password è: ${name + surname + color + number}`
+
+
+
+
+
+
+
+
+
+
+
